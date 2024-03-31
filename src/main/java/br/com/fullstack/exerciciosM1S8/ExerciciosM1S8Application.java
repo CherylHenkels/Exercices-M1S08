40794ae9706b6b2a8279c1468cbade415cb25dfa
CherylHenkels <cherylhenkelss@gmail.com>
2024-03-31 17:29:44 -0300

    M1S08: Ex 1 - Spring Initializer e repositório no GitHub

---
+++ src/main/java/br/com/fullstack/exerciciosM1S8/ExerciciosM1S8Application.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.exerciciosM1S8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciosM1S8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciosM1S8Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fullstack/exerciciosM1S8/ExerciciosM1S8ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.exerciciosM1S8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosM1S8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
