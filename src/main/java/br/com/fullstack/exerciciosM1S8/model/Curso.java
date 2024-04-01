a76cb98246c18fe90087c95b51ccd7bff3a4994b
CherylHenkels <cherylhenkelss@gmail.com>
2024-03-31 21:55:07 -0300

    M1S08: Ex 3 - Cursos

---
+++ src/main/java/br/com/fullstack/exerciciosM1S8/model/Curso.java
@@ -0,0 +1,31 @@
+// Exercicio 3 -> criação da classe Curso
+package br.com.fullstack.exerciciosM1S8.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class Curso {
+
+    private static Integer proximoId = 1;
+    private static List<Curso> cursosCadastrados = new ArrayList<>();
+
+    private int id;
+    @Setter private String nome;
+    @Setter private String descricao;
+    @Setter private Integer cargaHoraria;
+    
+
+    public static Curso controleID(Curso curso){
+        cursosCadastrados.add(curso);
+        curso.id = proximoId++;
+        return curso;
+    }
+
+
+
+}
